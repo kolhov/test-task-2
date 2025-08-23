da85fb41a4e05b64f0f8d844bc255964cf63b3ee
kolhov <kolhovv@ya.ru>
2025-08-23 15:46:33 +0400

    Initial commit

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import SideBar from '@/components/sidebar/SideBar.vue'
+import Sessions from '@/components/sessions/Sessions.vue'
+</script>
+
+<template>
+  <div class="wrap">
+    <SideBar />
+    <Sessions />
+  </div>
+</template>
+
+<style scoped>
+.wrap {
+  display: flex;
+  background-color: #f4f4f8;
+}
+</style>
